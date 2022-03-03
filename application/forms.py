b86c389de0aac3b0e59d1230fae58328a18550b5
Jack Middleton <jackmiddleton96@hotmail.co.uk>
2022-03-03 09:18:18 +0000

    created a basic dynamically filled form and added basic update functionality

--- application/forms.py
+++ application/forms.py
@@ -7,11 +7,12 @@ class CreateDM(FlaskForm):
     submit = SubmitField('Submit')
 
 class CreateNPC(FlaskForm):
-    dm_id = IntegerField('Associated DM ID')
+    dm_id = SelectField('Associated DM', choices=[])
+    npc_list = SelectField("NPC to update", choices=[])
     npc_name = StringField('Name of NPC')
     npc_race = SelectField('NPC Race', choices=[('Dragonborn', 'Dragonborn'), ('Dwarf','Dwarf'), ('Human', 'Human'), \
         ('Elf', 'Elf'), ('Half-Elf', 'Half-Elf'), ('Gnome', 'Gnome'), ('Halfling', 'Halfling'), ('Half-Orc', 'Half-Orc'), ('Human', 'Human'), \
             ('Tiefling', 'Tiefling')] )
     npc_details = StringField('Details of the NPC eg; equipment, basic stats etc.')
     submit = SubmitField('Submit')
-    
+
--- application/routes.py
+++ application/routes.py
@@ -7,12 +7,11 @@ from application.forms import CreateDM, CreateNPC
 def home():
     num_DMs = DM.query.count()
     num_NPCs = NPC.query.count()
-    dm_list = [str(dm) for dm in DM.query.all()]
-    NPC_list = [str(npc) for npc in NPC.query.all() ]
-    #dm_list = [str(DM) + " " + str(todo.project) for todo in Todo.query.all()]
-    return render_template('index.html', num = num_DMs, DMs = dm_list, no_NPCs = num_NPCs, NPCs = NPC_list)
-    
-    # and then in html - {{ to do }} in side a {% for %} {% end for %}
+    dm_list = list(DM.query.all())
+    npc_list = list(NPC.query.all())
+    dm_npcs = {dm:str([str(npc) for npc in dm.dm_npcs]) for dm in dm_list}
+    return render_template('index.html', num = num_DMs, npcs = npc_list, DMs = dm_list, no_NPCs = num_NPCs, dm_npc = dm_npcs)
+
 
 # search by table name
 # search by some detail from within a table ie; a DM name
@@ -48,12 +47,22 @@ def searchdm(details=None,field=None, id=None):
 def delete():
     return
 
-# update by table name, the column, find the ID that needs updating and then the new info
-# or correct all entries in a column for general typo's/spelling errors
-@app.route('/update/<keyword>/<field>/<id>/<newinfo>')
-@app.route('/update/<keyword>/<field>/<oldinfo>/<newinfo>')
-def update():
-    return
+@app.route('/updatenpc/<int:pk>', methods=['GET', 'POST'])
+def updatenpc(pk):
+    npc = NPC.query.get(pk)
+    dm = DM.query.all()
+    Form = CreateNPC()
+    Form.dm_id.choices.extend([( dm.dm_id, str(dm)) for dm in dm])
+    if request.method == 'POST':
+        npc.fk_dm_id = Form.dm_id.data
+        npc.npc_name = Form.npc_name.data
+        npc.npc_race = Form.npc_race.data
+        npc.npc_details = Form.npc_details.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('createNPC.html', form = Form, ptitle = "Update NPC")
+
+
 
 # create an entry in a database, uysing keyword as the table 
 # field as the field needing entry and its info
@@ -69,9 +78,13 @@ def createdm():
         return redirect(url_for('home'))
     return render_template('createDM.html', form = Form)
 
+
+
 @app.route('/createnpc', methods=['GET', 'POST'])
 def createnpc():
+    dm_id = DM.query.all()
     Form = CreateNPC()
+    Form.dm_id.choices.extend([( dm.dm_id, str(dm)) for dm in dm_id])
     if request.method == 'POST':
         fk_dm_id = Form.dm_id.data
         npc_name = Form.npc_name.data
@@ -82,3 +95,4 @@ def createnpc():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('createNPC.html', form = Form)
+
