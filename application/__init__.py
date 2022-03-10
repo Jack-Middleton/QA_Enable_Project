7a26dd2a397f0343c64a7553613e7d43fc2f62cf
Jack Middleton <jackmiddleton96@hotmail.co.uk>
2022-03-10 09:21:55 +0000

    Switched to production sql server

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dnd.db'
+#] app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dnd.db'
 # mysql+pymysql://<root username>:<root password>@<zip address>:3306/flask_demo_db where flask_demo_db is the name of the db in the virtual SQL instance
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('db_uri')
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = getenv('secret_key')
 
